b0b6190fceecb1b43f403e69bacfb4a2e9279587
Mirza Kobasevic <mkobasevic1@etf.unsa.ba>
2023-04-27 17:49:37 +0200

    implemented DFS search

--- zavrsni/src/App.js
+++ zavrsni/src/App.js
@@ -9,7 +9,7 @@ function Cell(props) {
 				props.column === 0 ? 'left' : ''
 			} ${props.column === 0 && props.row === 0 ? 'start' : ''} ${
 				props.row === props.endRow && props.column === props.endCol ? 'end' : ''
-			}`}></div>
+			} ${props.search?'search':''}`}></div>
 	);
 }
 
@@ -18,9 +18,13 @@ function Maze() {
 	const [visited, setVisited] = useState([]);
 	const [endRow, setEndRow] = useState();
 	const [endCol, setEndCol] = useState();
+	const [generated, setGenerated] = useState(false);
+	const [visited2,setVisited2]= useState([[false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]]);
 
 	// Create a new maze grid
 	const generateMaze = () => {
+		setGenerated(true);
+		setVisited2([[false,false,false,false],[false,false,false,false],[false,false,false,false],[false,false,false,false]]);
 		if (maze.length === 0) {
 			for (let i = 0; i < 4; i++) {
 				const row = [];
@@ -72,26 +76,73 @@ function Maze() {
 			dfs(nextRow, nextCol);
 		};
 
-		const startRow = 0; //Math.floor(Math.random() * 4);
-		const startCol = 0; //Math.floor(Math.random() * 4);
+		const startRow = 0; 
+		const startCol = 0; 
 		visited[startRow][startCol] = true;
 		maze[startRow][startCol].visited = true;
 		setVisited([...visited]);
 		dfs(startRow, startCol);
 	};
 
+	const solveBFS = () => {};
+	const solveDFS = (row,col) =>{
+		
+		const stack = [];		
+		if (row > 0 && !visited2[row - 1][col] && visited[row-1][col]) {		
+			stack.push([row - 1, col]);
+		}
+		if (col > 0 && !visited2[row][col - 1] && visited[row][col-1]) {
+			stack.push([row, col - 1]);
+		}
+		if (row < 3 && !visited2[row + 1][col] && visited[row+1][col]) {
+			stack.push([row + 1, col]);
+		}
+		if (col < 3 && !visited2[row][col + 1] && visited[row][col+1]) {
+			stack.push([row, col + 1]);
+		}
+		if (stack.length === 0 || (row === endRow && col === endCol)) {
+			
+			setTimeout(function(){
+				visited2[row][col]=true;
+				setVisited2([...visited2]);				
+				
+			}, 1000);
+			return;
+		}
+		
+		setTimeout(function(){
+			visited2[row][col]=true;
+			const [nextRow, nextCol] = stack.pop();
+			setVisited2([...visited2]);
+			setMaze([...maze]);
+			solveDFS(nextRow, nextCol);	
+		}, 1000);
+			
+	};
+
 	return (
-		<div className='maze'>
-			<button className='generateBtn' onClick={generateMaze}>
-				Generate Maze
-			</button>
-			{maze.map((row, i) => (
-				<div className='row' key={i}>
-					{row.map((cell, j) => (
-						<Cell key={i - j} row={i} column={j} visited={visited[i][j]} endRow={endRow} endCol={endCol} />
-					))}
-				</div>
-			))}
+		<div className='app'>
+			<div className='maze'>
+				<button className='generateBtn' onClick={generateMaze}>
+					Generate Maze
+				</button>
+				{maze.map((row, i) => (
+					<div className='row' key={i}>
+						{row.map((cell, j) => (
+							<Cell key={i - j} row={i} column={j} visited={visited[i][j]} endRow={endRow} endCol={endCol} search={visited2[i][j]}/>
+						))}
+					</div>
+				))}
+
+				{generated ? (
+					<div className='buttons'>
+						<button className='solve' onClick={event => solveDFS(0,0)}>Solve DFS</button>
+						<button className='solve' onclick={solveBFS}>Solve BFS</button>
+					</div>
+				) : (
+					''
+				)}
+			</div>
 		</div>
 	);
 }
